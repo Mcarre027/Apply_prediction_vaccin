f866026b4a58276e724f956682f2e110bb291452
Mcarre027 <carrehomebusiness@gmail.com>
2025-03-25 14:42:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -157,4 +157,5 @@ def update_explore(var):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=False, host="0.0.0.0", port=port)
